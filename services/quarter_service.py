1d765d5f8ebd4da659c1b4738caab43f0b26e66c
wstsws <wstswsb@gmail.com>
2022-06-29 14:47:53 +0700

    vector input; hardcode column and row

--- services/quarter_service.py
+++ services/quarter_service.py
@@ -8,22 +8,32 @@ class QuarterService:
     def double_round(self, matrix: list[list[int]]) -> list[list[int]]:
         return self.row_round(self.column_round(matrix))
 
-    def row_round(self, matrix: list[list[int]]) -> list[list[int]]:
+    def row_round(self, vector: list[int]) -> list[int]:
+        v = vector
+
+        z_0, z_1, z_2, z_3 = self.quarter_round(v[0], v[1], v[2], v[3])
+        z_5, z_6, z_7, z_4 = self.quarter_round(v[5], v[6], v[7], v[4])
+        z_10, z_11, z_8, z_9 = self.quarter_round(v[10], v[11], v[8], v[9])
+        z_15, z_12, z_13, z_14 = self.quarter_round(v[15], v[12], v[13], v[14])
+
         return [
-            self.quarter_round(*line)
-            for line
-            in matrix
+            z_0, z_1, z_2, z_3,
+            z_4, z_5, z_6, z_7,
+            z_8, z_9, z_10, z_11,
+            z_12, z_13, z_14, z_15
         ]
 
-    def column_round(self, matrix: list[list[int]]) -> list[list[int]]:
+    def column_round(self, vector: list[int]) -> list[int]:
+        v = vector
+        y_0, y_4, y_8, y_12 = self.quarter_round(v[0], v[4], v[8], v[12])
+        y_5, y_9, y_13, y_1 = self.quarter_round(v[5], v[9], v[13], v[1])
+        y_10, y_14, y_2, y_6 = self.quarter_round(v[10], v[14], v[2], v[6])
+        y_15, y_3, y_7, y_11 = self.quarter_round(v[15], v[3], v[7], v[11])
         return [
-            self.quarter_round(
-                matrix[0][i],
-                matrix[1][i],
-                matrix[2][i],
-                matrix[3][i],
-            )
-            for i in range(len(matrix))
+            y_0, y_1, y_2, y_3,
+            y_4, y_5, y_6, y_7,
+            y_8, y_9, y_10, y_11,
+            y_12, y_13, y_14, y_15
         ]
 
     def quarter_round(self, y_0: int, y_1: int, y_2: int, y_3: int) -> list[int]:
